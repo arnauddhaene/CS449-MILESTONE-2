ec61d7581a6a68c684dcd2c16df905042e657fad
arnauddhaene <arnaud.dhaene@epfl.ch>
2021-04-09 14:22:14 +0200

    Upload template

---
+++ src/main/scala/similarity/Predictor.scala
@@ -0,0 +1,129 @@
+package similarity
+
+import org.rogach.scallop._
+import org.json4s.jackson.Serialization
+import org.apache.spark.rdd.RDD
+
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val train = opt[String](required = true)
+  val test = opt[String](required = true)
+  val json = opt[String]()
+  verify()
+}
+
+case class Rating(user: Int, item: Int, rating: Double)
+
+object Predictor extends App {
+  // Remove these lines if encountering/debugging Spark
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  val spark = SparkSession.builder()
+    .master("local[1]")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  println("")
+  println("******************************************************")
+
+  var conf = new Conf(args)
+  println("Loading training data from: " + conf.train())
+  val trainFile = spark.sparkContext.textFile(conf.train())
+  val train = trainFile.map(l => {
+      val cols = l.split("\t").map(_.trim)
+      Rating(cols(0).toInt, cols(1).toInt, cols(2).toDouble)
+  })
+  assert(train.count == 80000, "Invalid training data")
+
+  println("Loading test data from: " + conf.test())
+  val testFile = spark.sparkContext.textFile(conf.test())
+  val test = testFile.map(l => {
+      val cols = l.split("\t").map(_.trim)
+      Rating(cols(0).toInt, cols(1).toInt, cols(2).toDouble)
+  })
+  assert(test.count == 20000, "Invalid test data")
+
+  // Save answers as JSON
+  def printToFile(content: String,
+                  location: String = "./answers.json") =
+    Some(new java.io.PrintWriter(location)).foreach{
+      f => try{
+        f.write(content)
+      } finally{ f.close }
+  }
+  conf.json.toOption match {
+    case None => ;
+    case Some(jsonFile) => {
+      var json = "";
+      {
+        // Limiting the scope of implicit formats with {}
+        implicit val formats = org.json4s.DefaultFormats
+        val answers: Map[String, Any] = Map(
+          "Q2.3.1" -> Map(
+            "CosineBasedMae" -> 0.0, // Datatype of answer: Double
+            "CosineMinusBaselineDifference" -> 0.0 // Datatype of answer: Double
+          ),
+
+          "Q2.3.2" -> Map(
+            "JaccardMae" -> 0.0, // Datatype of answer: Double
+            "JaccardMinusCosineDifference" -> 0.0 // Datatype of answer: Double
+          ),
+
+          "Q2.3.3" -> Map(
+            // Provide the formula that computes the number of similarity computations
+            // as a function of U in the report.
+            "NumberOfSimilarityComputationsForU1BaseDataset" -> 0 // Datatype of answer: Int
+          ),
+
+          "Q2.3.4" -> Map(
+            "CosineSimilarityStatistics" -> Map(
+              "min" -> 0.0,  // Datatype of answer: Double
+              "max" -> 0.0, // Datatype of answer: Double
+              "average" -> 0.0, // Datatype of answer: Double
+              "stddev" -> 0.0 // Datatype of answer: Double
+            )
+          ),
+
+          "Q2.3.5" -> Map(
+            // Provide the formula that computes the amount of memory for storing all S(u,v)
+            // as a function of U in the report.
+            "TotalBytesToStoreNonZeroSimilarityComputationsForU1BaseDataset" -> 0 // Datatype of answer: Int
+          ),
+
+          "Q2.3.6" -> Map(
+            "DurationInMicrosecForComputingPredictions" -> Map(
+              "min" -> 0.0,  // Datatype of answer: Double
+              "max" -> 0.0, // Datatype of answer: Double
+              "average" -> 0.0, // Datatype of answer: Double
+              "stddev" -> 0.0 // Datatype of answer: Double
+            )
+            // Discuss about the time difference between the similarity method and the methods
+            // from milestone 1 in the report.
+          ),
+
+          "Q2.3.7" -> Map(
+            "DurationInMicrosecForComputingSimilarities" -> Map(
+              "min" -> 0.0,  // Datatype of answer: Double
+              "max" -> 0.0, // Datatype of answer: Double
+              "average" -> 0.0, // Datatype of answer: Double
+              "stddev" -> 0.0 // Datatype of answer: Double
+            ),
+            "AverageTimeInMicrosecPerSuv" -> 0.0, // Datatype of answer: Double
+            "RatioBetweenTimeToComputeSimilarityOverTimeToPredict" -> 0.0 // Datatype of answer: Double
+          )
+         )
+        json = Serialization.writePretty(answers)
+      }
+
+      println(json)
+      println("Saving answers in: " + jsonFile)
+      printToFile(json, jsonFile)
+    }
+  }
+
+  println("")
+  spark.close()
+}
